c8cfebae3a6de43e006d7746c16efed3606c4f87
Stephen Nwaukwa <interprimos80@hotmail.com>
2021-07-27 08:09:06 -0500

    Update main page

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
-from appframe import NewFrame
-
-new_frame = NewFrame()
-
+from tkinter import *
+from tkinter import ttk
 
 
-new_frame()
+root = Tk()
+frame = ttk.Frame(root, width=500, height=500, padding=20)
+frame.grid(column=0, row=0)
+
+img_path = StringVar()
+img = ttk.Entry(frame, textvariable=img_path)
+img.grid(column=0, row=0, columnspan=2)
+
+img_label = StringVar()
+lbl = ttk.Label(frame, text="Upload image", padding=10)
+lbl.grid(column=3, row=0)
+
+
+
+
+
+
+
+
+
+root.mainloop()
