3a6b731967903e78bebb1f9b0c799ff0d24bc62d
Stephen Nwaukwa <interprimos80@hotmail.com>
2021-07-27 08:25:52 -0500

    Add app title

--- main.py
+++ main.py
@@ -2,24 +2,20 @@ from tkinter import *
 from tkinter import ttk
 
 
+def upload_image():
+    pass
+
+
 root = Tk()
-frame = ttk.Frame(root, width=500, height=500, padding=20)
+root.title("Stephen Watermark App")
+frame = ttk.Frame(root, width=500, height=500, padding=(20, 20, 50, 20))
 frame.grid(column=0, row=0)
 
 img_path = StringVar()
 img = ttk.Entry(frame, textvariable=img_path)
-img.grid(column=0, row=0, columnspan=2)
+img.grid(column=0, row=0, columnspan=9)
 
-img_label = StringVar()
-lbl = ttk.Label(frame, text="Upload image", padding=10)
-lbl.grid(column=3, row=0)
-
-
-
-
-
-
-
-
+img_upload = ttk.Button(frame, text="Upload image", command=upload_image)
+img_upload.grid(column=10, row=0)
 
 root.mainloop()
