9142a7085b670d2913abc4ac002cd17876b298ac
Stephen Nwaukwa <interprimos80@hotmail.com>
2021-07-31 00:25:07 -0500

    set up error handling upload_image fxn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter import ttk
+from tkinter import messagebox
 from PIL import ImageTk, Image
 
 root = Tk()
@@ -8,11 +9,19 @@ main_frame = ttk.Frame(root, padding=10)
 
 
 def upload_image():
+    """Receives image for processing; throws error for invalid entries"""
     image_path = image_entry.get()
-    watermarked_image = ImageTk.PhotoImage(Image.open(image_path))
-    image_display = Label(main_frame, image=watermarked_image)
-    image_display.image_names = watermarked_image  # needed for PhotoImage to work inside a function; not sure why
-    image_display.grid(column=0, row=1, columnspan=3)
+    global watermarked_image
+    try:
+        watermarked_image = ImageTk.PhotoImage(Image.open(image_path))
+    except AttributeError:
+        messagebox.showerror(title="Error!!", message="Image path is not valid!")
+    except FileNotFoundError:
+        messagebox.showerror(title="Error!!", message="No File Found!")
+    else:
+        image_display = Label(main_frame, image=watermarked_image)
+        # image_display.image_names = watermarked_image  # needed for PhotoImage to work inside a function; not sure why
+        image_display.grid(column=0, row=1, columnspan=3)
     return
 
 
@@ -21,12 +30,10 @@ image_address = StringVar()
 image_entry = ttk.Entry(main_frame, textvariable=image_address, width=75, justify="right")
 upload_button = ttk.Button(main_frame, text="Upload image", command=upload_image)
 
-
 # ========GEOMETRY MANAGER===================
 main_frame.grid(column=0, row=0)
 image_entry.grid(column=0, row=0)
 upload_button.grid(column=2, row=0)
-
 
 # ================RESIZING=====================
 root.columnconfigure(0, weight=1)
@@ -35,6 +42,10 @@ main_frame.columnconfigure(0, weight=1)
 main_frame.columnconfigure(1, weight=1)
 main_frame.columnconfigure(2, weight=1)
 main_frame.columnconfigure(3, weight=1)
+
+# TODO 2: Bind <Enter> to entry field
+# TODO 3: Create and add watermark to picture
+# TODO 4: Refactor code - functional programming
 
 
 root.mainloop()
