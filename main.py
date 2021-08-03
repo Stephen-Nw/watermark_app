02aee047c41be7db2d71953364c24f65b1560a7a
Stephen Nwaukwa <interprimos80@hotmail.com>
2021-08-02 22:23:41 -0500

    create canvas for image

--- main.py
+++ main.py
@@ -8,6 +8,22 @@ root.title("Stephen Watermark App")
 main_frame = ttk.Frame(root, padding=10)
 
 
+# def upload_image():
+#     """Receives image for processing; throws error for invalid entries"""
+#     image_path = image_entry.get()
+#     global watermarked_image
+#     try:
+#         watermarked_image = ImageTk.PhotoImage(Image.open(image_path))
+#     except AttributeError:
+#         messagebox.showerror(title="Error!!", message="Image path is not valid!")
+#     except FileNotFoundError:
+#         messagebox.showerror(title="Error!!", message="No File Found!")
+#     else:
+#         image_display = Label(main_frame, image=watermarked_image)
+#         # image_display.image_names = watermarked_image  # needed for PhotoImage to work inside a function; not sure why
+#         image_display.grid(column=0, row=1, columnspan=3)
+
+
 def upload_image():
     """Receives image for processing; throws error for invalid entries"""
     image_path = image_entry.get()
@@ -19,9 +35,13 @@ def upload_image():
     except FileNotFoundError:
         messagebox.showerror(title="Error!!", message="No File Found!")
     else:
-        image_display = Label(main_frame, image=watermarked_image)
-        # image_display.image_names = watermarked_image  # needed for PhotoImage to work inside a function; not sure why
-        image_display.grid(column=0, row=1, columnspan=3)
+        canvas = Canvas(main_frame, background='red', width=550, height=550)
+        # image_display = Label(canvas, image=watermarked_image)
+        canvas.grid(column=0, row=1, columnspan=3)
+        # image_display.grid(column=0, row=1, columnspan=3)
+        canvas.create_image(10, 10, image=watermarked_image, anchor="nw")
+        canvas.create_text(100, 500, text="welcome", font=('Helvetica', 25, "bold"), fill='white')
+
 
 
 
@@ -60,7 +80,6 @@ main_frame.columnconfigure(1, weight=1)
 main_frame.columnconfigure(2, weight=1)
 main_frame.columnconfigure(3, weight=1)
 
-# TODO 2: Bind <Enter> to entry field
 # TODO 3: Create and add watermark to picture
 # TODO 4: Refactor code - functional programming
 
