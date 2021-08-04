06c008d7226f3568913a1298ab721dc0c6b73626
Stephen Nwaukwa <interprimos80@hotmail.com>
2021-08-03 21:54:41 -0500

    code refactor

--- main.py
+++ main.py
@@ -11,22 +11,6 @@ main_frame = ttk.Frame(root, padding=10)
 watermarkFont = Font(family="Brush Script MT", size=35, weight="normal", slant="italic")
 
 
-# def upload_image():
-#     """Receives image for processing; throws error for invalid entries"""
-#     image_path = image_entry.get()
-#     global watermarked_image
-#     try:
-#         watermarked_image = ImageTk.PhotoImage(Image.open(image_path))
-#     except AttributeError:
-#         messagebox.showerror(title="Error!!", message="Image path is not valid!")
-#     except FileNotFoundError:
-#         messagebox.showerror(title="Error!!", message="No File Found!")
-#     else:
-#         image_display = Label(main_frame, image=watermarked_image)
-#         # image_display.image_names = watermarked_image  # needed for PhotoImage to work inside a function; not sure why
-#         image_display.grid(column=0, row=1, columnspan=3)
-
-
 def upload_image():
     """Receives image for processing; throws error for invalid entries"""
     image_path = image_entry.get()
@@ -38,14 +22,10 @@ def upload_image():
     except FileNotFoundError:
         messagebox.showerror(title="Error!!", message="No File Found!")
     else:
-        canvas = Canvas(main_frame, background='red', width=550, height=550)
-        # image_display = Label(canvas, image=watermarked_image)
+        canvas = Canvas(main_frame, width=550, height=550)
         canvas.grid(column=0, row=1, columnspan=3)
-        # image_display.grid(column=0, row=1, columnspan=3)
         canvas.create_image(10, 10, image=watermarked_image, anchor="nw")
         canvas.create_text(100, 500, text="interprimos", font=watermarkFont, fill='white')
-
-
 
 
 def enter(event):
@@ -59,10 +39,11 @@ def enter(event):
     except FileNotFoundError:
         messagebox.showerror(title="Error!!", message="No File Found!")
     else:
-        image_display = Label(main_frame, image=watermarked_image)
-        # image_display.image_names = watermarked_image  # needed for PhotoImage to work inside a function; not sure why
-        image_display.grid(column=0, row=1, columnspan=3)
-    return
+        canvas = Canvas(main_frame, width=550, height=550)
+        canvas.grid(column=0, row=1, columnspan=3)
+        canvas.create_image(10, 10, image=watermarked_image, anchor="nw")
+        canvas.create_text(100, 500, text="interprimos", font=watermarkFont, fill='white')
+
 
 # ===========CREATE OBJECTS====================
 image_address = StringVar()
@@ -83,10 +64,8 @@ main_frame.columnconfigure(1, weight=1)
 main_frame.columnconfigure(2, weight=1)
 main_frame.columnconfigure(3, weight=1)
 
-# TODO 3: Create and add watermark to picture
+
 # TODO 4: Refactor code - functional programming
-
-
 
 
 root.mainloop()
